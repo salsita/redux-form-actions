4ebab2d3cc8062d701948c8dc0eacd8cf7d9c534
Roman Krejcik <farin@farin.cz>
2016-10-25 18:51:17 +0200

    fixed immutable import

--- src/immutable.js
+++ src/immutable.js
@@ -1,6 +1,6 @@
 import * as ReduxForm from 'redux-form/immutable';
-import wrapper from '../wrapper';
-import { reduxFormMiddleware } from '../middlewares';
+import wrapper from './wrapper';
+import { reduxFormMiddleware } from './middlewares';
 
 module.exports = {
   ...ReduxForm,
